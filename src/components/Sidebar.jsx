fc51d26f493754e28013795d67773cab643ad157
himanxu1 <technophilic000@gmail.com>
2023-12-31 13:26:54 +0530

    logout-refactor

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
 
   const handleLogout = () =>{
     localStorage.removeItem('token')
-    navigate('/login')
+    navigate('/')
   }
   return (
     <div className="fixed  ml-10 w-[280px] h-[550px] shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset] m-2">
